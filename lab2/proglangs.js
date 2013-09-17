97ba279c9c0602bf5fa4e82b20e23aa887d9e32f
Ola Malnes <ola.malnes@gmail.com>
2013-09-17 20:58:53 +0200

    lab2 del2
    Jeg har slått sammen index.html fra første deloppgave med proglangs og
    javascript fra forrige lab. Javascript er endret fra lab1 etter som jeg
    ikke hadde mulighet til å endre tekst med forrige versjon.

---
+++ lab2/proglangs.js
@@ -0,0 +1,7 @@
+var element_unordered_list = document.getElementById("first7");alert(element_unordered_list);element_unordered_list.style.color = "green";
+
+function printToOutputBox(selectobj){
+	var e_proglangs = document.getElementById("proglangs");
+	var e_outputbox = document.getElementById("outputbox");
+	e_outputbox.innerHTML = "Du har selektert programmeringsspråket " + e_proglangs.options[selectobj.selectedIndex].text;
+ }
